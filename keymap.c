de594160cd2eb64043b9fc080fc115b3047a3c74
bilalairfan <irfanahmedbilal@gmail.com>
2026-03-29 19:25:30 -0400

    Update keymap.c

--- keymap.c
+++ keymap.c
@@ -9,19 +9,43 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
 };
 #endif
 
+#ifdef OLED_ENABLE
+bool oled_task_user(void) {
+// Modifier status
+    uint8_t mods = get_mods();
+    oled_write_P(PSTR("Mods: "), false);
+    
+    oled_write_P(mods & MOD_MASK_CTRL ? PSTR("CTL ") : PSTR("    "), false);
+    oled_write_P(mods & MOD_MASK_SHIFT ? PSTR("SFT ") : PSTR("    "), false);
+    oled_write_P(mods & MOD_MASK_ALT ? PSTR("ALT ") : PSTR("    "), false);
+    
+    oled_write_ln_P(PSTR(""), false); // empty line
+    
+
+    // Host Keyboard LED Status
+    led_t led_state = host_keyboard_led_state();
+    oled_write_P(led_state.num_lock ? PSTR("NUM ") : PSTR("    "), false);
+    oled_write_P(led_state.caps_lock ? PSTR("CAP ") : PSTR("    "), false);
+    oled_write_P(led_state.scroll_lock ? PSTR("SCR ") : PSTR("    "), false);
+    
+    return false;
+}
+#endif
+
+
+
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /*
-     * ┌───┬───┬───┬───┐
-     * │ 7 │ 8 │ 9 │ / │
-     * ├───┼───┼───┼───┤
-     * │ 4 │ 5 │ 6 │ * │
-     * ├───┼───┼───┼───┤
-     * │ 1 │ 2 │ 3 │ - │
-     * ├───┼───┼───┼───┤
-     * │ 0 │ . │Ent│ + │
-     * └───┴───┴───┴───┘
+     * ┌───┬───┬───┬
+     * │ec │ 8 │ 9 │
+     * ├───┼───┼───┼
+     * │ 4 │ 5 │ 6 │
+     * ├───┼───┼───┼
+     * │ 1 │ 2 │ 3 │
+     * ├───┼───┼───┼
+     *
      */
     [0] = LAYOUT(
         KC_NO,   KC_B,   KC_E, 
